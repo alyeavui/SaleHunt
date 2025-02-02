37e134572bccbdb71edb6e8c4e57fc1a0b4349ae
Ayaulym Zhuniskhan <ayaulym.zhuniskhan89@gmail.com>
2025-02-03 00:12:22 +0500

    Starting message

---
+++ SaleHunt/db_handler/db_class.py
@@ -0,0 +1,10 @@
+# db_handler/db_class.py
+
+class PostgresHandler:
+    def __init__(self, dsn):
+        self.dsn = dsn
+        # Ваш код для подключения к базе данных
+
+    def connect(self):
+        # Подключение к базе данных
+        pass
---
+++ SaleHunt/handlers/start.py
@@ -0,0 +1,9 @@
+from aiogram import Router, F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+
+start_router = Router()
+
+@start_router.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer('Привет! Я твой помощник 🌿')
